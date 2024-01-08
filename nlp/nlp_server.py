92a0794263fc760a850af03c98becaa8209dffa8
omcs-i-a <omcsiy1@gmail.com>
2024-01-09 02:27:29 +0900

    feat: curlでpostした文字列をそのまま返す機能を作成
    nlp_server.pyでcorrect_text関数を定義し、curlでjson形式{'text': '内容'}でpostするとそのまま返す機能を作成した。

---
+++ nlp/nlp_server.py
@@ -0,0 +1,23 @@
+# import nlp_module
+from flask import Flask, jsonify, request
+
+
+app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+
+
+@app.route('/')
+def index():
+    return "Hello, nlp"
+
+
+@app.route('/correct', methods=['POST'], endpoint="correct-text")
+def correct_text():
+    data = request.get_json()
+    text = data['text']
+    corrected_text = text
+    return jsonify({'corrected_text': corrected_text})
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
